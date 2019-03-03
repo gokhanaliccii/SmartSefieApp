05bbdd38cad422458f05401019bc18cc4c016a6b
gokhanaliccii <gokhanaliccii@gmail.com>
2019-03-03 12:25:05 +0300

    Start camera by the camera permission status

--- app/src/main/java/com/onfido/android/techtask/CameraActivity.kt
+++ app/src/main/java/com/onfido/android/techtask/CameraActivity.kt
@@ -1,7 +1,12 @@
 package com.onfido.android.techtask
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import io.fotoapparat.Fotoapparat
 import io.fotoapparat.log.logcat
 import io.fotoapparat.log.loggers
@@ -11,6 +16,10 @@ import io.fotoapparat.view.CameraView
 
 class CameraActivity : AppCompatActivity() {
 
+    companion object {
+        const val CAMERA_PERMISSION_REQUEST = 1
+    }
+
     private lateinit var cameraView: CameraView
     private val fotoapparat: Fotoapparat by lazy { createFotoapparat() }
 
@@ -19,7 +28,6 @@ class CameraActivity : AppCompatActivity() {
         setContentView(R.layout.activity_camera)
 
         cameraView = findViewById(R.id.camera_view)
-        createFotoapparat()
     }
 
     // Ref(Step Two): https://github.com/RedApparat/Fotoapparat
@@ -37,11 +45,47 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        fotoapparat.start()
+
+        if (hasCameraPermission()) {
+            fotoapparat.start()
+        } else {
+            requestForCameraPermission()
+        }
     }
 
     override fun onStop() {
         super.onStop()
-        fotoapparat.stop()
+
+        if (hasCameraPermission()) {
+            fotoapparat.stop()
+        }
+    }
+
+    private fun cameraPermissionPermitted() {
+        fotoapparat.start()
+    }
+
+    private fun hasCameraPermission(): Boolean =
+        ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PERMISSION_GRANTED
+
+    private fun requestForCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // currently we had only camera permission
+        if (CAMERA_PERMISSION_REQUEST == requestCode && grantResults.isNotEmpty()) {
+            cameraPermissionPermitted()
+        }
     }
 }
